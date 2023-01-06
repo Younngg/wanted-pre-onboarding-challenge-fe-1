5a04862e65484e29528ccead0a39fbfdb6715c22
younngg <8533283@naver.com>
2023-01-06 16:45:15 +0900

    design: 회원가입 페이지 margin 수정

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import RegisterForm from '../components/RegisterForm/RegisterForm';
 import { PageTitle } from '../styles/page';
 
 const Register = () => {
   return (
-    <>
+    <Container>
       <PageTitle>회원가입</PageTitle>
       <RegisterForm />
-    </>
+    </Container>
   );
 };
 
 export default Register;
+
+const Container = styled.div`
+  margin: auto;
+`;
